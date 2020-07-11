a3ef21cbf9251e3847a4c13e7d49844ed84e2964
Theo Vora <theo.vora@gmail.com>
2020-07-11 17:52:12 -0600

    Add Key.go method

--- AOC2016-D02/Models/Key.swift
+++ AOC2016-D02/Models/Key.swift
@@ -42,3 +42,78 @@ enum Key {
         }
     }
 }
+
+extension Key {
+    func go(_ dir: Direction) -> Key {
+        switch dir {
+        case .up:
+            switch self {
+            case .k1, .k2, .k3:
+                return self
+            case .k4:
+                return .k1
+            case .k5:
+                return .k2
+            case .k6:
+                return .k3
+            case .k7:
+                return .k4
+            case .k8:
+                return .k5
+            case .k9:
+                return .k6
+            }
+        case .down:
+            switch self {
+            case .k1:
+                return .k4
+            case .k2:
+                return .k5
+            case .k3:
+                return .k6
+            case .k4:
+                return .k7
+            case .k5:
+                return .k8
+            case .k6:
+                return .k9
+            case .k7, .k8, .k9:
+                return self
+            }
+        case .left:
+            switch self {
+            case .k1, .k4, .k7:
+                return self
+            case .k2:
+                return .k1
+            case .k3:
+                return .k2
+            case .k5:
+                return .k4
+            case .k6:
+                return .k5
+            case .k8:
+                return .k7
+            case .k9:
+                return .k8
+            }
+        case .right:
+            switch self {
+            case .k3, .k6, .k9:
+                return self
+            case .k1:
+                return .k2
+            case .k2:
+                return .k3
+            case .k4:
+                return .k5
+            case .k5:
+                return .k6
+            case .k7:
+                return .k8
+            case .k8:
+                return .k9
+            }
+        }
+    }
+}
